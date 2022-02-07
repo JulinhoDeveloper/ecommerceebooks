25801918b304b589ac0bf2233fcc9fc1f0cfcc5b
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-02-06 22:07:40 -0300

    corrigi o erro de id único e criei o controller de usuários

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {register} = require('../controllers/user');
+
+router.post('/registro', register);
 
 module.exports = router;
