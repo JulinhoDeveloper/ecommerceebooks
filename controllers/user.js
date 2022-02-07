5750d89091d1d9ae73b27722fcb9a14637040d52
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-02-06 23:26:47 -0300

    registrando usuário

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
 const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+
 const { errorHandler } = require('../helpers/dbErrorHandler');
+
 
 exports.register = async (req, res) => {
     try {
@@ -16,3 +19,29 @@ exports.register = async (req, res) => {
         console.error(err.message);
     }
 };
+
+exports.login = (req, res) => {
+    // verifica se o eamil existe 
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'Não tem esse email cadastrado'
+            });
+        }
+        // se o eamil existe verifica se a senha confere com a cadastrada
+    
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email ou senha não conferem'
+            });
+        }
+        // gera o token do usuário
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        // cadastra usando o token e o tempo de expiração
+        res.cookie('t', token, { expire: new Date() + 9999 });
+        //retorna o token e o user no front end
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
+    });
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {register} = require('../controllers/user');
+const {register, login} = require('../controllers/user');
 const {userRegistroValidator} = require('../validator/index')
 
 router.post('/registro', userRegistroValidator, register);
+router.post('/login', userRegistroValidator, login);
 
 module.exports = router;
