a5696139797723b3b2ed599ea3c1f52dd9651dbb
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-02-06 22:35:35 -0300

    cadastrando usuários

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,18 @@
 const User = require('../models/user')
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
-
-exports.register = (req,res) =>{
-
-    
+exports.register = async (req, res) => {
+    try {
+        const user = await new User(req.body);
+        await user.save((err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.status(200).json({ user });
+        });
+    } catch (err) {
+        console.error(err.message);
+    }
 };
