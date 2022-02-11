97a30ea3dd403f2926b6c6a3de97d9294e2338dd
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-02-10 23:41:28 -0300

    cadastrando usuários

--- front/src/user/Login.js
+++ front/src/user/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../core/Layout.js'
 
 const Login = () => (
+    
     <Layout title="Login Page" description="E-commerce">
         
     </Layout>
--- front/src/user/Registro.js
+++ front/src/user/Registro.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import Layout from '../core/Layout.js'
+import React, { useState } from 'react';
+import Layout from '../core/Layout'
+//import {API} from '../config'
 
-const Registro = () => (
-    <Layout title="Registro Page" description="E-commerce">
-        
+const Registro = () => {
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    }
+
+    const registroForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Nome</label>
+                <input onChange={handleChange('name') } 
+                type="text" 
+                className="form-control"/>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email') } 
+                type="email" 
+                className="form-control"/>
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">Senha</label>
+                <input onChange={handleChange('password') } 
+                type="password" 
+                className="form-control"/>
+            </div>
+            
+            <button className="btn btn-primary" >Cadastrar</button>
+        </form>
+    )
+
+    return (
+        <Layout title="Registro Page" 
+        description="E-commerce"
+        className="container col-md-8 offset-md-2"
+        >
+            { registroForm() }
+            { JSON.stringify(values) }
     </Layout>
+    )
+}
+  
 
-);
+
 
 
 export default Registro;
